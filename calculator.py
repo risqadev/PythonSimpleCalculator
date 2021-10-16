d46b5434137c5a6050778e6bdc09b77495a7b32b
Ricardo Camacho <rscamachodev@gmail.com>
2021-10-16 19:46:52 -0300

    funções renomeadas

--- calculator.py
+++ calculator.py
@@ -1,53 +1,53 @@
 def menu():
-    print()
-    print(" Python Calculator ".center(50, '#'))
-
     select = 'n'
     while select != '0':
-        select = input("\nOperações:\n"
+        select = input("\n" +
+                       " Python Calculator ".center(50, '#') +
+                       "\n\nOperações:\n"
                        "1 - Adição\n"
                        "2 - Subtração\n"
                        "3 - Multiplicação\n"
                        "4 - Divisão\n"
-                       "0 - Sair\n"
+                       "0 - Sair\n\n"
                        "Escolha uma opção (1/2/3/4/0): ")
 
         if select == '1':
             print('\nAdição')
-            op = getOp()
-            printExpressao(select, *op)
-            print(sum(convertArrayToFloat(op)))
+            operators = getOperators()
+            printExpression(select, *operators)
+            print(sum(convertArrayToFloat(operators)))
         elif select == '2':
             print('\nSubtração')
-            op = getOp()
-            printExpressao(select, *op)
-            print(subtracao(*convertArrayToFloat(op)))
+            operators = getOperators()
+            printExpression(select, *operators)
+            print(subtraction(*convertArrayToFloat(operators)))
         elif select == '3':
             print('\nMultiplicação')
-            op = getOp()
-            printExpressao(select, *op)
-            print(multiplicacao(*convertArrayToFloat(op)))
+            operators = getOperators()
+            printExpression(select, *operators)
+            print(multiplication(*convertArrayToFloat(operators)))
         elif select == '4':
             print('\nDivisão')
-            op = getOp()
-            printExpressao(select, *op)
-            print(divisao(*convertArrayToFloat(op)))
+            operators = getOperators()
+            printExpression(select, *operators)
+            print(division(*convertArrayToFloat(operators)))
         elif select == '0':
             print('\nSair')
         else:
             print('Opção inválida.')
 
 
-def getOp():
-    op = []
-    op.append(input('Informe o 1º operando: '))
-    op.append(input('Informe o 2º operando: '))
+def getOperators():
+    operators = []
+    operators.append(input('Informe o 1º operando: '))
+    operators.append(input('Informe o 2º operando: '))
     i = 2
-    while op[i-1] != 'c':
-        op.append(input('Informe o %sº operando (ou "c" para calcular): ' % (i+1)))
+    while operators[i-1] != 'c':
+        operators.append(
+            input('Informe o %sº operando (ou "c" para calcular): ' % (i+1)))
         i += 1
-    op.pop()
-    return op
+    operators.pop()
+    return operators
 
 
 def convertArrayToFloat(list):
@@ -56,7 +56,7 @@ def convertArrayToFloat(list):
     return list
 
 
-def printExpressao(codOp, op1, *ops):
+def printExpression(codOp, op1, *ops):
     sinais = ['+', '-', 'x', '÷']
     sinal = sinais[int(codOp) - 1]
     print('\n%s' % op1, end=' ')
@@ -65,21 +65,21 @@ def printExpressao(codOp, op1, *ops):
     print('=', end=' ')
 
 
-def subtracao(op1, op2, *ops):
+def subtraction(op1, op2, *ops):
     result = op1 - op2
     for n in ops:
         result -= n
     return result
 
 
-def multiplicacao(op1, op2, *ops):
+def multiplication(op1, op2, *ops):
     result = op1 * op2
     for n in ops:
         result *= n
     return result
 
 
-def divisao(op1, op2, *ops):
+def division(op1, op2, *ops):
     result = op1 / op2
     for n in ops:
         result /= n
